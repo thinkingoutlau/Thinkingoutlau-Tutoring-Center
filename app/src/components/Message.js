0cc410385e33dac4bb4b380b4aee54a018f5617f
Winnie Lau <thinkingoutlau@gmail.com>
2022-11-09 13:04:06 -0500

    changes

--- app/src/components/Message.js
+++ app/src/components/Message.js
@@ -59,43 +59,7 @@ const NewMessageEntry = () => {
         </div>
       )}
     </div>
-    // <div>
-    //   <li className="media">
-    //     <div className="media-left">
-    //       {/* <a href="#"> */}
-    //       {/* <img className="media-object" src={"winnie"} alt="image" /> */}
-    //       {/* </a> */}
-    //     </div>
-    //     <div className="media-body">
-    //       <h4 className="media-heading">Winnie</h4>
-    //     </div>
-    //   </li>
-    //   <form
-    //     id="new-message-form"
-    //     onSubmit={(event) => {
-    //       event.preventDefault();
-    //       handleSubmit(message);
-    //     }}
-    //   >
-    //     <div className="input-group input-group-lg">
-    //       <input
-    //         className="form-control"
-    //         type="text"
-    //         name="content"
-    //         onChange={(event) =>
-    //           setMessage({ ...message, message: event.target.value })
-    //         }
-    //         value={message.message}
-    //         placeholder="Say something nice..."
-    //       />
-    //       <span className="input-group-btn">
-    //         <button className="btn btn-default" type="submit">
-    //           Chat!
-    //         </button>
-    //       </span>
-    //     </div>
-    //   </form>
-    // </div>
+
   );
 };
 
