7212115d4adad859f56ab35e26f056ccf5c2fe3a
Winnie Lau <thinkingoutlau@gmail.com>
2022-11-09 23:37:34 -0500

    edited description of tutors

--- app/src/components/Tutors.js
+++ app/src/components/Tutors.js
@@ -21,9 +21,12 @@ function Tutors() {
         <div>
           <img src="https://media-exp1.licdn.com/dms/image/D4E03AQGRv0Hc51JqEA/profile-displayphoto-shrink_800_800/0/1666814894227?e=1673481600&v=beta&t=b13VaHjov-KGMjlDtTjd_Ke3IXdY0QsK3wUNQCHFeqU" />
           <div className="description">
-            Hi there! My name is Nikki and I am from Brooklyn, New York. I am
-            one of the tutors at Thinkingoutlau Tutoring Center. I tutor Algebra
-            1, 2, and Geometry. Feel free to ask me anything!
+            Hi there! My name is Nikki and I am from Brooklyn, New York. I have
+            a bachelor's degree in Business Administration, specifically in
+            Marketing and Management. I work at Walmart as a Business
+            Development Manager. I am also one of the tutors at Thinkingoutlau
+            Tutoring Center. I tutor Algebra 1, 2, and Geometry. Feel free to
+            ask me anything!
           </div>
         </div>
       </div>
@@ -32,9 +35,12 @@ function Tutors() {
         <div>
           <img src="https://media-exp1.licdn.com/dms/image/C4D03AQG14IpmevaGXg/profile-displayphoto-shrink_800_800/0/1655726228547?e=1670457600&v=beta&t=FcqTJfLci7zf_QstwqMVYjEuErR2zcQe6RT62F5dwEw" />
           <div className="description">
-            Hi mate! I'm Rosemary from Syndey, Australia and I am one of the
-            tutors at Thinkingoutlau Tutoring Center. I tutor Precalculus and
-            Calculus. I love maths so ask me anything you like!
+            Hi mate! I'm Rosemary from Syndey, Australia and I have a bachelor's
+            degree in Accounting and Applied Finance. I am a Senior Consultant
+            at Uber and I tutor at Thinkingoutlau Tutoring Center as well. I
+            have experience teaching young adults -- I had volunteered abroad in
+            Taiwan and Poland. I tutor Precalculus and Calculus. I love maths so
+            ask me anything you like!
           </div>
         </div>
       </div>
